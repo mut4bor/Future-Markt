d8c8c8aa70f0b58788b23ebd42a71957417f91d4
mut4bor <matvei.molchanov2004@gmail.com>
2024-03-14 01:24:45 +0300

    added mixins for blur and transition

--- src/entities/header/index.tsx
+++ src/entities/header/index.tsx
@@ -1,12 +1,17 @@
-import styled from './style.module.scss';
+import styles from './style.module.scss';
 import { HeaderNavigation } from './header-navigation';
 import { HeaderTelephone } from './header-telephone';
 import { HeaderLogo } from './header-logo';
+import { useAppSelector } from 'shared/api/redux/hooks';
 
 export function Header() {
+  const { sideMenuVisible } = useAppSelector((state) => state.sideMenu);
+
   return (
-    <header className={styled.header}>
-      <div className={styled.container}>
+    <header
+      className={`${styles.header} ${sideMenuVisible ? styles.blur : ''}`}
+    >
+      <div className={styles.container}>
         <HeaderLogo />
         <HeaderNavigation />
         <HeaderTelephone />
